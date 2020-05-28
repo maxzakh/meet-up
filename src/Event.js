e822bd444158e81a6f5b158f70c5f704d6eef4f2
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-05-27 19:10:21 -0700

    Testing scenarios 1

---
+++ src/Event.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Event extends Component {
+    // state = {
+    //     events: [],
+    // }
+
+    // render() {
+    //     return (
+    //         <ul className="EventList">
+    //             {this.state.events.map(event =>
+    //                 <li>
+    //                     <Event event={event} />
+    //                 </li>
+    //             )}
+    //         </ul>
+    //     );
+    // }
+}
+
+export default Event;
