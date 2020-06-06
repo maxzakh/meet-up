0c3a80275f178790507e9f5e3082de305bdd66c3
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-06-06 01:07:46 -0700

    Added way to change # visible events

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
     state = {
-        query: 32
+        query: 11
     };
 
-    
+    handleInputChanged = (event) => {
+        const value = event.target.value;
+        this.setState({ query: value });
+        if (value) {
+            this.props.updateEvents(null, null, value);
+        }
+    };
 
     render() {
         return (
-            <div className='numberOfEvents'>{this.state.query}</div>
+            <div className='numberOfEvents'>
+                Displaying: {this.state.query} Events
+                <input
+                    className='number-of-events'
+                    type='number'
+                    onChange={this.handleInputChanged}
+                    value={this.state.query}
+                    placeholder='32'
+                />
+            </div>
         )
     }
 }
