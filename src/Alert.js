9bd5d99e1ef7c7ada5aeb9650baa759117bc8a39
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-06-16 00:55:20 -0700

    Added alert if offline

--- src/Alert.js
+++ src/Alert.js
@@ -28,11 +28,11 @@ class InfoAlert extends Alert {
     }
 }
 
-class ErrorAlert extends Alert {
+class OfflineAlert extends Alert {
     constructor(props) {
         super(props);
         this.color = 'red';
     }
 }
 
-export { InfoAlert, ErrorAlert };
+export { InfoAlert, OfflineAlert };
--- src/App.js
+++ src/App.js
@@ -4,28 +4,45 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents } from './api';
+import { OfflineAlert } from './Alert';
 
 class App extends Component {
   state = {
     events: [],
-    page: null
+    page: null,
+    offlineText: '',
   }
 
   componentDidMount() {
     this.updateEvents();
+    window.addEventListener('online', this.offLineAlert());
   }
+
+  offLineAlert = () => {
+    if (!navigator.onLine) {
+      this.setState({
+        offlineText:
+          'No internet connetion, please reconnect to update event information. This is a cached list',
+      });
+    } else {
+      this.setState({
+        offlineText: '',
+      });
+    }
+  };
+
 
   updateEvents = (lat, lon, page) => {
     if (lat && lon) {
-      getEvents(lat, lon, this.state.page).then(events => 
+      getEvents(lat, lon, this.state.page).then(events =>
         this.setState({ events, lat, lon })
       );
     } else if (page) {
       getEvents(this.state.lat, this.state.lon, page).then(events =>
-        this.setState({ events, page})
+        this.setState({ events, page })
       );
     } else {
-      getEvents(this.state.lat, this.state.lon, this.state.page).then(events => 
+      getEvents(this.state.lat, this.state.lon, this.state.page).then(events =>
         this.setState({ events })
       );
     }
@@ -34,7 +51,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CitySearch updateEvents={this.updateEvents} />
+        {!window.navigator.online
+          ?
+          (<OfflineAlert text={this.state.offlineText} />)
+          :
+          (<CitySearch updateEvents={this.updateEvents} />)
+        }
         <EventList events={this.state.events} />
         <NumberOfEvents updateEvents={this.updateEvents} />
       </div>
