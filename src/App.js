1cc29d2c6e67c4f38ecffef13d765bb461e59d04
Max Zakharzhevskiy <maxzakh@ucdavis.edu>
2020-06-22 23:43:01 -0700

    Fixed onLine typo

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {!window.navigator.online
+        {!window.navigator.onLine
           ?
           (<OfflineAlert text={this.state.offlineText} />)
           :
